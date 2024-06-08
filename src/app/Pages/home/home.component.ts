62b01e7c522a3a6bb54043335d767a1130027f18
Stephen Appah <appahstephen9@gmail.com>
2024-06-08 07:56:12 +0000

    Feat: WOrking on Contact Page

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ContactComponent } from "../../Components/contact/contact.component";
 import { NavBarComponent } from '../../Components/nav-bar/nav-bar.component';
 import { HeroComponent } from "../../Components/hero/hero.component";
 import { FooterComponent } from '../../Components/footer/footer.component';
@@ -8,7 +9,7 @@ import { FooterComponent } from '../../Components/footer/footer.component';
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [NavBarComponent, HeroComponent, FooterComponent,]
+    imports: [NavBarComponent, HeroComponent, FooterComponent, ContactComponent]
 })
 export class HomeComponent {
 
